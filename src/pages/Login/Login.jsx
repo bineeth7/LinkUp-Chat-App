f20c27910b4279aa0151ad38986f90c46e25ab55
Bineeth B C <bineethbc7@gmail.com>
2024-08-16 19:20:37 +0530

    login here, click here logic applied

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -20,7 +20,11 @@ function Login() {
                 <p>Agree to the terms of use & privacy policy.</p>
             </div>
             <div className="login-forgot">
-                <p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>Click here</span></p>
+                {
+                    currState === "Sign up"
+                    ?<p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>Login here</span></p>
+                    :<p className="login-toggle">Create an account <span onClick={()=>setCurrState("Sign up")}>Click here</span></p>
+                }
             </div>
         </form>
 
