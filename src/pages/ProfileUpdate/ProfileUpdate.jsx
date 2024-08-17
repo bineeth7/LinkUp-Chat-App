4b0bf446633b1bc8c40805c780007a34b97c21b7
Bineeth B C <bineethbc7@gmail.com>
2024-08-17 20:44:53 +0530

    profile design complete, need to add logic

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,24 @@
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 function ProfileUpdate() {
   return (
-    <div></div>
+    <div className='profile'>
+        <div className="profile-container">
+          <form>
+              <h3>Profile Details</h3>
+              <label htmlFor="avatar">
+                <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+                <img src={assets.avatar_icon} alt="" />
+                upload profile image
+              </label>
+              <input type="text" placeholder='Your name' required />
+              <textarea placeholder='Write profile bio' required></textarea>
+              <button type='submit'>Save</button>
+          </form>
+          <img className='icon' src={assets.logo_icon} alt="" />
+        </div>
+    </div>
   )
 }
 
