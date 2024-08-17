b888bf49cc4365d73aac011b40b73dd21dc403fc
Bineeth B C <bineethbc7@gmail.com>
2024-08-17 14:16:59 +0530

    RightSidebar UI added

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,9 +1,26 @@
 import './RightSidebar.css'
-
+import assets from '../../assets/assets'
 function RightSidebar() {
   return (
-    <div>
-        
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>Bineeth B C <img src={assets.green_dot} className='dot' alt="" /></h3>
+        <p>Hey, There I'm using LinkUp</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
     </div>
   )
 }
