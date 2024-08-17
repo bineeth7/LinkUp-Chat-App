1473fa1833787b023a50a09350f9a2f25a90be4b
Bineeth B C <bineethbc7@gmail.com>
2024-08-17 20:53:00 +0530

    profile pic selecting logic added

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,15 +1,19 @@
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
+import { useState } from 'react'
 
 function ProfileUpdate() {
+
+  const [image, setImage] = useState(false);
+
   return (
     <div className='profile'>
         <div className="profile-container">
           <form>
               <h3>Profile Details</h3>
               <label htmlFor="avatar">
-                <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
-                <img src={assets.avatar_icon} alt="" />
+                <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+                <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
                 upload profile image
               </label>
               <input type="text" placeholder='Your name' required />
